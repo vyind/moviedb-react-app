ca793de348f0af348328095cc5dfb332cf78d5e5
vysk <vy.ind19@gmail.com>
2020-08-02 20:14:27 +0530

    fix query parsing issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import Category from "./movies/[category]";
+import Link from "next/link";
 
 const fetchUrls = {};
 
 export default function Index() {
   return (
     <div>
+      <Link as={"/movies/popular"} href={"/movies/[category]"}>
+        <a>
+          <h2>Popular Movies</h2>
+        </a>
+      </Link>
       <Category useHorizontal={true} categoryName="popular" />
+      <Link as="/movies/top_rated" href="/movies/[category]">
+        <a>
+          <h2>Top-Rated Movies</h2>
+        </a>
+      </Link>
       <Category useHorizontal={true} categoryName="top_rated" />
     </div>
   );
--- src/pages/movies/[category].tsx
+++ src/pages/movies/[category].tsx
@@ -1,11 +1,19 @@
 import { MovieList, Result } from "../../../api/movieList";
 import ItemList from "../../components/itemList";
 import { useState, useEffect } from "react";
+import { NextPageContext } from "next";
+import { useRouter } from "next/router";
 
 interface PopularListProps {
   moviesPopular?: Result[];
   useHorizontal: boolean;
   categoryName: string;
+}
+
+interface CustomNextPageContext extends NextPageContext {
+  query: {
+    category: string;
+  };
 }
 
 const fetchUrls = {
@@ -15,12 +23,14 @@ const fetchUrls = {
     "https://api.themoviedb.org/3/movie/top_rated?api_key=4b3d06492e3ec908d60ebda3525a807f",
 };
 
-export default function Category({
+function Category({
   moviesPopular,
   useHorizontal,
   categoryName,
 }: PopularListProps) {
   const [movies, setMovie] = useState(moviesPopular);
+  const router = useRouter();
+  if (!categoryName) categoryName = router.query["category"].toString();
   useEffect(() => {
     async function loadData() {
       const response = await fetch(fetchUrls[categoryName]);
@@ -42,10 +52,12 @@ export default function Category({
   );
 }
 
-Category.getInitialProps = async ({ req, query }) => {
-  if (!req) return { moviesPopular: {} };
+Category.getInitialProps = async ({ query, req }: CustomNextPageContext) => {
+  if (!req || !query) return { moviesPopular: {} };
   const response = await fetch(fetchUrls[query.category]);
   const apiData: MovieList | undefined = await response.json();
   const moviesPopular: Result[] | undefined = apiData.results;
   return { moviesPopular: moviesPopular, categoryName: query.category };
 };
+
+export default Category;
