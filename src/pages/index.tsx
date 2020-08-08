519a6736b2a331a0a9de634d79930089ba29d41a
vysk <vy.ind19@gmail.com>
2020-08-08 21:19:18 +0530

    debug deployment issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ import ShowCategory from "./tv/[category]";
 export default function Index() {
   return (
     <div>
-      <MovieCategory useHorizontal={true} categoryName="trending" />
+      {/* <MovieCategory useHorizontal={true} categoryName="trending" />
       <MovieCategory useHorizontal={true} categoryName="popular" />
       <ShowCategory useHorizontal={true} categoryName="trending" />
-      <ShowCategory useHorizontal={true} categoryName="popular" />
+      <ShowCategory useHorizontal={true} categoryName="popular" /> */}
     </div>
   );
 }
